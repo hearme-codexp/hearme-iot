a7ced732990f9dfd32c9ca9297caeb802dac84df
Paolo Bueno <paolohaji@gmail.com>
2018-03-08 21:38:13 -0300

    Adicionar envio de msg quando comecarVibrar()

--- Vibra__o1.0/Vibra__o1.0.ino
+++ Vibra__o1.0/Vibra__o1.0.ino
@@ -1,39 +1,60 @@
+#include "SoftwareSerial.h"
+#include <ArduinoJson.h>
 int anterior = 0;
 int microphone = A0;
-int viber = 3;
+int viber = 5;
 unsigned long inicioVibracao;
 
 #define DURACAO_VIBRACAO 300
 #define SENSIBILIDADE 100
 #define INTERVALO_LEITURA 20
 
-void setup() {
-  // put your setup code here, to run once:
+StaticJsonBuffer<100> jsonBuffer;
+SoftwareSerial bluetooth(2, 3);
+void setup()
+{
   pinMode(viber, OUTPUT);
   pinMode(microphone, INPUT);
   Serial.begin(9600);
-  // vibrar = map(vibrar, 0, 1023, 0, 255);
+  bluetooth.begin(115200);
 }
-void loop() {
+void loop()
+{
   int now = analogRead(microphone);
   Serial.println(now);
   delay(INTERVALO_LEITURA);
-  if (now > anterior + SENSIBILIDADE) {
+  if (now > anterior + SENSIBILIDADE)
+  {
     comecarVibrar();
   }
 
   // determinar se é pra parar
-  if (inicioVibracao > 0 && inicioVibracao + DURACAO_VIBRACAO < millis()) {
+  if (inicioVibracao > 0 && inicioVibracao + DURACAO_VIBRACAO < millis())
+  {
     pararVibrar();
   }
 
   anterior = now;
 }
 
-void comecarVibrar() {
+void enviarHistoricoVibracao()
+{
+  // formato da mensagem pode ser {"v":1}
+  JsonObject &json = jsonBuffer.createObject();
+  json["v"] = 1;
+
+  // a biblioteca ArduinoJson já imprime para uma porta Serial/SoftwareSerial
+  json.printTo(bluetooth);
+}
+
+void comecarVibrar()
+{
   inicioVibracao = millis();
   digitalWrite(viber, HIGH);
+  enviarHistoricoVibracao();
 }
-void pararVibrar() {
+void pararVibrar()
+{
   digitalWrite(viber, LOW);
   inicioVibracao = 0;
+}
